03afc43c7478199b848d0f264a616a7e1ae24e8f
Glenn Hevey <glenn.hevey@gmail.com>
2019-08-27 17:46:31 +1000

    Setup multi-target added xamarin.mac added netstandard2.0

---
+++ Pastel.Core/Platform/Window/Window.netstandard.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Pastel.Core.Window
+{
+    public partial class Window
+    {
+        internal void CreateWindow()
+        {
+            Console.WriteLine("Hello from NetStandard");
+        }
+    }
+}
